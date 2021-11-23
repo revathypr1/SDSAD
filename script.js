b98bb0774e6a9ad23cb109bdce376ce08491dda1
revathypr <revathypr75@gmail.com>
2021-11-23 18:02:07 +0530

    SDSAD

---
+++ script.js
@@ -0,0 +1,7 @@
+const calc=function(){
+    var a=prompt("enter the first number")
+    var b=prompt("enter the second number")
+    num=parseInt(a)+parseInt(b)
+    return (num)    
+}
+alert (calc())
